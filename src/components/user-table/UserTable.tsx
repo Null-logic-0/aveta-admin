777a8b0b8fa20330c31d68ac09fd2ef91cf10754
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-08-07 20:45:03 +0400

    Update userTable

--- src/components/user-table/UserTable.tsx
+++ src/components/user-table/UserTable.tsx
@@ -2,6 +2,7 @@ import { Table } from "antd";
 import Heading from "../UI/Heading";
 import AppResult from "../UI/Result";
 
+import Empty from "../UI/Empty";
 import { columns } from "./user-table-columns";
 import { useFetchAllUsers } from "../../hooks/useFetchAllUsers";
 
@@ -18,20 +19,23 @@ function UserTable() {
       </div>
     );
 
+  if (users.length === 0) {
+    return <Empty description={"No Registered Users!"} />;
+  }
+
   const userDataArray = Array.isArray(users?.data) ? users.data : [];
   return (
     <div className="flex flex-col gap-6  items-center justify-center w-full">
       <Heading title="All Users" isTitle />
-      {!users && (
-        <Table
-          columns={columns}
-          dataSource={userDataArray}
-          rowKey="id"
-          loading={isPending}
-          className="w-full"
-          pagination={{ pageSize: 10 }}
-        />
-      )}
+
+      <Table
+        columns={columns}
+        dataSource={userDataArray}
+        rowKey="id"
+        loading={isPending}
+        className="w-full"
+        pagination={{ pageSize: 10 }}
+      />
     </div>
   );
 }
