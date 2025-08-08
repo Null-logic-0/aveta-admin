d4b632044c5481b13020f48d01b30105cbe8113c
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-08-08 12:26:59 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/": {
-        target: "http://ec2-13-218-165-221.compute-1.amazonaws.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
